f9c25ddccf5fa24a1b929ea591784d55e11072b6
Gopichandar Selvaraj <gopichandarbe@gmail.com>
2023-01-28 15:08:21 +0530

    Auth added

--- BlazorKiteConnect/src/Client/Pages/Index.razor.cs
+++ BlazorKiteConnect/src/Client/Pages/Index.razor.cs
@@ -4,12 +4,6 @@ namespace BlazorKiteConnect.Client.Pages
 {
     public partial class Index
     {
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-
-        private void Login()
-        {
-            NavigationManager.NavigateTo("/api/login/authorize", true);
-        }
+        
     }
 }
--- BlazorKiteConnect/src/Client/Program.cs
+++ BlazorKiteConnect/src/Client/Program.cs
@@ -1,4 +1,6 @@
 using BlazorKiteConnect.Client;
+using BlazorKiteConnect.Client.Infrastructure.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -9,5 +11,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+// cookie
+builder.Services
+    .AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>()    
+    .AddAuthorizationCore();    
 
 await builder.Build().RunAsync();
